e0e36e1e09fc88af8d46e6bfca95f7943ddfd115
Diana Asmus <dianaasmus@dianas-air.speedport.ip>
2023-03-21 21:58:45 +0100

    foundation structure

---
+++ script.js
@@ -0,0 +1,34 @@
+let questions = [
+    {
+        "question": "Who invented HTML?",
+        "answer_1": "Robbie WIlliams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "What does the HTML tag <a> mean?",
+        "answer_1": "bold text",
+        "answer_2": "container",
+        "answer_3": "a link",
+        "answer_4": "italics",
+        "right_answer": 3
+    },
+    {
+        "question": "How to insert a link to an external website in HTML?",
+        "answer_1": "With the <a> and the href attribute",
+        "answer_2": "With the <link> and the attribute href",
+        "answer_3": "Mit dem <img> und dem Attribut src",
+        "answer_4": "With the <div> and the class attribute",
+        "right_answer": 1
+    },
+    {
+        "question": "How to create a table in HTML?",
+        "answer_1": "Mit <div> and class on table",
+        "answer_2": "With <form> and method on table",
+        "answer_3": "With <table>, <thead>, <tbody> and <tfoot> </tfoot>",
+        "answer_4": "With <img> and src on the image file of the table",
+        "right_answer": 3
+    },
+];
