981adcc9ea71eaf8792f401cee39b2dff2705693
Diana Asmus <dianaasmus@dianas-air.speedport.ip>
2023-03-22 13:16:49 +0100

    first Category: first question

--- script.js
+++ script.js
@@ -32,3 +32,57 @@ let questions = [
         "right_answer": 3
     },
 ];
+
+function init() {
+    let rightSide = document.getElementById('right');
+
+    rightSide.innerHTML = `
+            <h1>Welcome to <br>
+            THEQUIZAPP for Web Developer!</h1>
+            <p class="start-q">Ready for the Challenge?</p>
+            <button class="start-btn" onclick="startQuiz()">START</button>
+    `;
+}
+
+function startQuiz() {
+    let rightSide = document.getElementById('right');
+
+    rightSide.innerHTML = returnQuestionsContainer();
+
+    document.getElementById('question-nr').innerHTML = questions.length;
+}
+
+
+function returnQuestionsContainer() {
+    return `
+    <h2>Category: HTML</h2>
+    <p class="amounts">1 / <span id="question-nr"></span></p>
+    <h2>Which prgramming language is used the most?</h2>
+    <div class="answers">
+        <div>
+            <h3>
+                <img src="img/empty-circle.png">
+                HTML
+            </h3>
+            <h3>
+                <img src="img/empty-circle.png">
+                CSS
+            </h3>
+        </div>
+        <div>
+            <h3>
+                <img src="img/empty-circle.png">
+                Java
+            </h3>
+            <h3>
+                <img src="img/empty-circle.png">
+                JavaScript
+            </h3>
+        </div>
+    </div>
+    <div>
+        <img src="img/arrow.png" class="arrow">
+        <img src="img/arrow.right.png" class="arrow">
+    </div>
+    `;
+}
