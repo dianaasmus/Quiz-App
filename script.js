2934ce2cc5613bf7d5922240b30337f1225e024e
Diana Asmus <dianaasmus@dianas-air.speedport.ip>
2023-03-23 23:09:45 +0100

    next Btn + End screen

--- script.js
+++ script.js
@@ -1,54 +1,65 @@
 let questions = [
     {
-        "question": "Who invented HTML?",
-        "answer_1": "Robbie WIlliams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
-        "right_answer": 3
+        "question": "When was the first programming language developed?",
+        "answer_1": "1943",
+        "answer_2": "1954",
+        "answer_3": "1929",
+        "answer_4": "1892",
+        "right_answer": 2
     },
     {
-        "question": "What does the HTML tag <a> mean?",
-        "answer_1": "bold text",
-        "answer_2": "container",
-        "answer_3": "a link",
-        "answer_4": "italics",
-        "right_answer": 3
-    },
-    {
-        "question": "How to insert a link to an external website in HTML?",
-        "answer_1": "With the <a> and the href attribute",
-        "answer_2": "With the <link> and the attribute href",
-        "answer_3": "Mit dem <img> und dem Attribut src",
-        "answer_4": "With the <div> and the class attribute",
+        "question": "Who was the first known 'programmer'?",
+        "answer_1": "Ada Lovelace",
+        "answer_2": "Ken Thompson",
+        "answer_3": "Niklaus Wirth",
+        "answer_4": "Grace Hopper",
         "right_answer": 1
     },
     {
-        "question": "How to create a table in HTML?",
-        "answer_1": "Mit <div> and class on table",
-        "answer_2": "With <form> and method on table",
-        "answer_3": "With <table>, <thead>, <tbody> and <tfoot> </tfoot>",
-        "answer_4": "With <img> and src on the image file of the table",
-        "right_answer": 3
+        "question": "which programming language consists only of +, ., >, <, -, [, ] and ,?",
+        "answer_1": "Whitespace",
+        "answer_2": "Brainfuck",
+        "answer_3": "INTERCAL",
+        "answer_4": "JSFUCK",
+        "right_answer": 2
     },
+    {
+        "question": "What was the first programming language?",
+        "answer_1": "PL/1 (Programming Language One)",
+        "answer_2": "LISP (LISt Processing)",
+        "answer_3": "Ada (named after Ada Lovelace)",
+        "answer_4": "Fortran (Formula Translation)",
+        "right_answer": 4
+    },
+    {
+        "question": "Which programming language can children and adults use to programme in a playful way?",
+        "answer_1": "Nintendo KEY",
+        "answer_2": "THE SIMS BUILD",
+        "answer_3": "UNO ONLINE",
+        "answer_4": "LEGO MINDSTORMS",
+        "right_answer": 4
+    }
 ];
 
 let currentQuestion = 0;
 
 function init() {
-    let rightSide = document.getElementById('right');
+    let rightSide = document.getElementById('main');
 
     rightSide.innerHTML = `
-            <h1>Welcome to <br>
-            THEQUIZAPP for Web Developer!</h1>
-            <p class="start-q">Ready for the Challenge?</p>
+            <p class="start-p">WELCOME TO</p>
+            <div class="main-logo" onclick="init()">
+            <img src="img/quizApp-logo.png" class="main-logo-p1">
+            <img src="img/thequizapp.png" class="main-logo-p2">
+            </div>
             <button class="start-btn" onclick="startQuiz()">START</button>
     `;
 }
 
 function startQuiz() {
-    let rightSide = document.getElementById('right');
+    let rightSide = document.getElementById('main');
 
+    document.getElementById('logo').style = '';
     rightSide.innerHTML = returnQuestionsContainer();
 
     document.getElementById('question-nr').innerHTML = questions.length;
@@ -58,35 +69,30 @@ function startQuiz() {
 
 function returnQuestionsContainer() {
     return `
-    <h2>Category: HTML</h2>
+    
     <p class="amounts"><span id="current-question-nr"></span> / <span id="question-nr"></span></p>
     <h2 id="seen-question"></h2>
     <div class="answers">
         <div>
             <h3 onclick="answer('answer_1')">
-                <img src="img/empty-circle.png">
+                
                 <span id="answer_1"></span>
             </h3>
             <h3 onclick="answer('answer_2')">
-                <img src="img/empty-circle.png">
                 <span id="answer_2"></span>
             </h3>
         </div>
         <div>
             <h3 onclick="answer('answer_3')">
-                <img src="img/empty-circle.png">
                 <span id="answer_3"></span>
             </h3>
             <h3 onclick="answer('answer_4')">
-                <img src="img/empty-circle.png">
                 <span id="answer_4"></span>
             </h3>
         </div>
     </div>
     <div>
-        <img src="img/arrow.png" class="arrow" id="previous_question" disable>
-        <img type="button" src="img/arrow.right.png" class="arrow" id="next_question" onclick="nextQuestion()" disable>
-        
+        <button class="next-btn" id="next_question" onclick="nextQuestion()" disabled>NEXT</button>
     </div>
     `;
 }
@@ -94,17 +100,21 @@ function returnQuestionsContainer() {
 function showCurrentQuestion() {
 
     if (currentQuestion >= questions.length) {
-        // TODO show End Screen
+        document.getElementById('main').innerHTML = `
+            <p class="result">1 / 5</p>
+            <p class="congratulations">Congratulations</p>
+            <img class="win" src="img/win.png">
+            <button class="play-again-btn" onclick="playAgain()">PLAY AGAIN</button>`;
     } else {
-    
-    let question = questions[currentQuestion];
-    document.getElementById('current-question-nr').innerHTML = currentQuestion + 1;
 
-    document.getElementById('seen-question').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+        let question = questions[currentQuestion];
+        document.getElementById('current-question-nr').innerHTML = currentQuestion + 1;
+
+        document.getElementById('seen-question').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
     }
 }
 
